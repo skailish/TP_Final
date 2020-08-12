1a1a9fb39880ccb83ddf675b1e903dbb27b4dfdb
Josefina del Rey <delreyjo@gmail.com>
2020-08-12 20:32:28 -0300

    agrego data a tvseries

--- src/pages/TVSeries.js
+++ src/pages/TVSeries.js
@@ -15,39 +15,41 @@ const TVSeries = () => {
     year,
   } = useContext(TvShowContext);
   return (
-    <Container className="main-container">
-      <Hero
-        data={dataTvShowRandom}
-        year={year}
-        voteAverage={voteAverage}
-        mediaType="tv"
-      />
-      <CardListPreview
-        mediaType="tv"
-        data={dataTvShow}
-        sectionTitle="Popular TV Shows"
-        category="popular"
-      />
+    dataTvShow && (
+      <Container className="main-container">
+        <Hero
+          data={dataTvShowRandom}
+          year={year}
+          voteAverage={voteAverage}
+          mediaType="tv"
+        />
+        <CardListPreview
+          mediaType="tv"
+          data={dataTvShow}
+          sectionTitle="Popular TV Shows"
+          category="popular"
+        />
 
-      <CardListPreview
-        mediaType="tv"
-        data={dataTvTop}
-        sectionTitle="Top Rated TV Shows"
-        category="top_rated"
-      />
-      <CardListPreview
-        mediaType="tv"
-        data={dataCurrentTv}
-        sectionTitle="Currently Airing TV Shows"
-        category="on_the_air"
-      />
-      <CardListPreview
-        mediaType="tv"
-        data={dataTodayTv}
-        sectionTitle="TV Shows Airing Today"
-        category="airing_today"
-      />
-    </Container>
+        <CardListPreview
+          mediaType="tv"
+          data={dataTvTop}
+          sectionTitle="Top Rated TV Shows"
+          category="top_rated"
+        />
+        <CardListPreview
+          mediaType="tv"
+          data={dataCurrentTv}
+          sectionTitle="Currently Airing TV Shows"
+          category="on_the_air"
+        />
+        <CardListPreview
+          mediaType="tv"
+          data={dataTodayTv}
+          sectionTitle="TV Shows Airing Today"
+          category="airing_today"
+        />
+      </Container>
+    )
   );
 };
 
