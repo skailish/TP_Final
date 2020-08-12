a7aba69aee9580b3398f6f3a734f92d736e8cd3b
Angie Turne <angie.turne@hotmail.com>
2020-08-12 18:44:06 -0300

    comienzo pagina movies

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,35 +1,7 @@
-import React, { useContext } from "react";
-import MovieContext from "../contexts/MovieContext";
-import ThemeContext from "../contexts/ThemeContext";
-
-import Footer from "../components/Footer";
-import Hero from "../components/Hero";
-import Container from "../components/primitive/Container";
-
-import Button from "../components/primitive/Button";
+import React from "react";
 
 const Movie = () => {
-  const { dataMovieRandom, yearMovie, voteAverageMovie } = useContext(
-    MovieContext
-  );
-
-  const { theme } = useContext(ThemeContext);
-  console.log(dataMovieRandom);
-  return (
-    dataMovieRandom && (
-      <Container className="movie-container">
-        <Hero
-          data={dataMovieRandom}
-          year={yearMovie}
-          voteAverage={voteAverageMovie}
-          mediaType="movie"
-        />
-        <Container className={`container-details-movie ${theme}`}></Container>
-
-        <Footer />
-      </Container>
-    )
-  );
+  return <div></div>;
 };
 
 export default Movie;
