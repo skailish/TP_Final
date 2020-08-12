3f87abd14cb793aaa726f241d5d3edb8b2360377
Josefina del Rey <delreyjo@gmail.com>
2020-08-12 16:58:06 -0300

    footer provisorio

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,75 +1,96 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "./primitive/Container";
 import Heading from "./primitive/Heading";
+import Link from "./primitive/Link";
+import Text from "./primitive/Text";
 import { GithubSquare } from "@styled-icons/fa-brands/GithubSquare";
 import { LinkedinSquare } from "@styled-icons/boxicons-logos/LinkedinSquare";
+import ThemeContext from "../contexts/ThemeContext";
 //import { Mail } from "@styled-icons/entypo/Mail";
 
 const Footer = () => {
+  const { theme } = useContext(ThemeContext);
+  const yearCopyright = new Date().getFullYear();
   return (
-    <Container className="footer-container">
-      <Container className="footer-container-personal">
-        <Heading className="footer-heading" level={3}>
-          Daniela
-        </Heading>
-        <Container className="footer-icons-container">
-          <GithubSquare
-            className="footer-icon github-icon"
-            target="_blank"
-            href="https://gitlab.com/d-capponi"
-          />
-          <LinkedinSquare
-            className="footer-icon"
-            target="_blank"
-            href="https://www.linkedin.com/in/danielacapponi/"
-          />
-        </Container>
+    <Container className={`footer-container ${theme}`}>
+      <Container className={`footer-rights-container ${theme}`}>
+        <Text>
+          © {yearCopyright} Designed and built by Angie Turne - Daniela Capponi
+          - Josefina del Rey - Lujan Sanchez. All rights reserved.
+        </Text>
+        <Text>
+          Data provided by
+          <Link target="_blank" href="https://www.themoviedb.org/">
+            TMDb.
+          </Link>
+        </Text>
       </Container>
-      <Container className="footer-container-personal">
-        <Heading className="footer-heading" level={3}>
-          Lujan
-        </Heading>
-        <Container className="footer-icons-container">
-          <GithubSquare
-            className="footer-icon github-icon"
-            target="_blank"
-            href="https://github.com/LoohanZero"
-          />
-          <LinkedinSquare
-            className="footer-icon"
-            target="_blank"
-            href="https://www.linkedin.com/in/luján-sanchez-6493ba70/"
-          />
+      <Container className="personal-data-container">
+        <Container className="footer-container-personal">
+          <Heading className={`footer-heading ${theme}`} level={3}>
+            Daniela
+          </Heading>
+          <Container className="footer-icons-container">
+            <Link target="_blank" href="https://gitlab.com/d-capponi">
+              <GithubSquare className={`footer-icon github-icon ${theme}`} />
+            </Link>
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/danielacapponi/"
+            >
+              <LinkedinSquare className={`footer-icon ${theme}`} />
+            </Link>
+          </Container>
         </Container>
-      </Container>
-      <Container className="footer-container-personal">
-        <Heading className="footer-heading" level={3}>
-          Angie
-        </Heading>
-        <Container className="footer-icons-container">
-          <GithubSquare
-            className="footer-icon github-icon"
-            target="_blank"
-            href="https://github.com/angieTu"
-          />
-          <LinkedinSquare className="footer-icon" target="_blank" href="" />
+        <Container className="footer-container-personal">
+          <Heading className={`footer-heading ${theme}`} level={3}>
+            Lujan
+          </Heading>
+
+          <Container className="footer-icons-container">
+            <Link target="_blank" href="https://github.com/LoohanZero">
+              <GithubSquare className={`footer-icon github-icon ${theme}`} />
+            </Link>
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/luján-sanchez-6493ba70/"
+            >
+              <LinkedinSquare className={`footer-icon ${theme}`} />
+            </Link>
+          </Container>
         </Container>
-      </Container>
-      <Container className="footer-container-personal">
-        <Heading className="footer-heading" level={3}>
-          Josefina
-        </Heading>
-        <Container className="footer-icons-container">
-          <GithubSquare
-            className="footer-icon github-icon"
-            target="_blank"
-            href="https://github.com/Jodelrey"
-          />
-          <LinkedinSquare
-            className="footer-icon"
-            target="_blank"
-            href="https://www.linkedin.com/in/josefina-del-rey/"
-          />
+        <Container className="footer-container-personal">
+          <Heading className={`footer-heading ${theme}`} level={3}>
+            Angie
+          </Heading>
+          <Container className="footer-icons-container">
+            <Link target="_blank" href="https://github.com/angieTu">
+              <GithubSquare className={`footer-icon github-icon ${theme}`} />
+            </Link>
+            <Link target="_blank" href="">
+              <LinkedinSquare
+                className={`footer-icon ${theme}`}
+                target="_blank"
+                href=""
+              />
+            </Link>
+          </Container>
+        </Container>
+        <Container className="footer-container-personal">
+          <Heading className={`footer-heading ${theme}`} level={3}>
+            Josefina
+          </Heading>
+          <Container className="footer-icons-container">
+            <Link target="_blank" href="https://github.com/Jodelrey">
+              <GithubSquare className={`footer-icon github-icon ${theme}`} />
+            </Link>
+            <Link
+              target="_blank"
+              href="https://www.linkedin.com/in/josefina-del-rey/"
+            >
+              <LinkedinSquare className={`footer-icon ${theme}`} />
+            </Link>
+          </Container>
         </Container>
       </Container>
     </Container>
