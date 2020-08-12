25f6f4b69d8821fc8c89da9f6101fbac09576007
Lujan Sanchez <mlulasan@gmail.com>
2020-08-12 20:19:07 -0300

    modifico footer para ajustar ancho de página

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Aside from "./components/Aside";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Container from "./components/primitive/Container"
 
 function App() {
   return (
