80542d4a42cd66ca8e05214ee4535412f46be9d9
Josefina del Rey <delreyjo@gmail.com>
2020-08-13 10:33:31 -0300

    mergeo con series

--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Container from "../../components/primitive/Container";
 
 const Categories = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <Container className="main-container">
+      <h1>Categories!!!</h1>
+    </Container>
+  );
+};
 
-export default Categories
+export default Categories;
