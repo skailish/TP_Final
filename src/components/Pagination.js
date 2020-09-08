5ac725a435fb62af6697fa8bcdb3f19977a21a77
d-capponi <capponi.daniela@gmail.com>
2020-09-08 19:59:52 -0300

    subo avances en paginación, sin terminar...

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -11,192 +11,73 @@ import ThemeContext from "../contexts/ThemeContext";
 
 const Pagination = ({ page, maxPage, setPage }) => {
   const { theme } = useContext(ThemeContext);
-
   const toPreviousPage = () => (page !== 1 ? setPage(page - 1) : setPage(page));
-
   const toNextPage = () => {
     page !== maxPage ? setPage(page + 1) : setPage(page);
   };
-
   const paginas = maxPage > 6 ? 6 : maxPage;
 
+  const getPagination = (maxPage, page, paginas) => {
+    // const pagination = Array.from({ length: 100 }, (_, i) => i + 1);
+    const pagination = [];
+    if (page > 1) {
+      pagination.push(<ButtonPagination
+        className={ `pagination-arrow` }
+        onClick={ () => toPreviousPage() }
+      ><ArrowIosBackOutline className={ "arrows" } />
+      </ButtonPagination>)
+    }
+    if (paginas >= 6 && page < 5) {
+      [...Array(5)].map((pageBtn, i) => {
+        pagination.push([i + 1, i + 1, i + 1], [maxPage - 3, maxPage - 3, "..."], [maxPage, maxPage, maxPage])
+      })
+    }
+       
+    if (paginas >= 6 && page >= 5 && page < maxPage - 4) {
+      pagination.push([1,1,1],[3,3,"..."],[page - 1,page - 1,page - 1],[page,page,page],[page + 1,page + 1,page + 1],[maxPage - 3,maxPage - 3,"..."],[maxPage,maxPage,maxPage])
+    }
+    if (paginas >= 6 && page >= maxPage - 4) {
+    pagination.push([ 1,1,1],[4,4,"..."],[maxPage - 4,maxPage - 4,maxPage - 4],[maxPage - 3,maxPage - 3,maxPage - 3],[maxPage - 2,maxPage - 2,maxPage - 2],[maxPage - 1,maxPage - 1,maxPage - 1],[maxPage,maxPage,maxPage])}
+    if (paginas < 6) {
+      [...Array(paginas - 1)].map((pageBtn, i) => (
+           pagination.push([
+          i + 1,
+          i + 1,
+          i + 1
+        ],
+        [
+          maxPage,
+          maxPage,
+          maxPage
+        ]
+      )))
+    }
+         
+    if (page < maxPage) {
+      pagination.push(
+        <ButtonPagination
+          className={ `pagination-arrow` }
+          onClick={ () => toNextPage() }
+        >
+          <ArrowIosForwardOutline className={ "arrows" } />
+        </ButtonPagination>)
+         
+    }
+
+  } 
+  const pagination = getPagination(maxPage, page, paginas);
+  console.log(pagination);
   return (
     <>
-      {paginas > 1 && (
-        <Container className={`pagination-container ${theme}`}>
-          {page > 1 && (
-            <ButtonPagination
-              className={`pagination-arrow`}
-              onClick={() => toPreviousPage()}
-            >
-              <ArrowIosBackOutline className={"arrows"} />
-            </ButtonPagination>
-          )}
-          {paginas >= 6 && page < 5 && (
-            <>
-              {[...Array(5)].map((pageBtn, i) => (
-                <PageButton
-                  setPage={setPage}
-                  page={page}
-                  key={i + 1}
-                  value={i + 1}
-                  content={i + 1}
-                />
-              ))}
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 3}
-                key={maxPage - 3}
-                content={"..."}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage}
-                key={maxPage}
-                content={maxPage}
-              />
-            </>
-          )}
-          {paginas >= 6 && page >= 5 && page < maxPage - 4 && (
-            <>
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={1}
-                key={1}
-                content={1}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={3}
-                key={3}
-                content={"..."}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={page - 1}
-                key={page - 1}
-                content={page - 1}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={page}
-                key={page}
-                content={page}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={page + 1}
-                key={page + 1}
-                content={page + 1}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 3}
-                key={maxPage - 3}
-                content={"..."}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage}
-                key={maxPage}
-                content={maxPage}
-              />
-            </>
-          )}
-          {paginas >= 6 && page >= maxPage - 4 && (
-            <>
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={1}
-                key={1}
-                content={1}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={4}
-                key={4}
-                content={"..."}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 4}
-                key={maxPage - 4}
-                content={maxPage - 4}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 3}
-                key={maxPage - 3}
-                content={maxPage - 3}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 2}
-                key={maxPage - 2}
-                content={maxPage - 2}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage - 1}
-                key={maxPage - 1}
-                content={maxPage - 1}
-              />
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage}
-                key={maxPage}
-                content={maxPage}
-              />
-            </>
-          )}
-          {paginas < 6 && (
-            <>
-              {[...Array(paginas - 1)].map((pageBtn, i) => (
-                <PageButton
-                  setPage={setPage}
-                  page={page}
-                  key={i + 1}
-                  value={i + 1}
-                  content={i + 1}
-                />
-              ))}
-              <PageButton
-                setPage={setPage}
-                page={page}
-                value={maxPage}
-                key={maxPage}
-                content={maxPage}
-              />
-            </>
-          )}
-          {page < maxPage && (
-            <ButtonPagination
-              className={`pagination-arrow`}
-              onClick={() => toNextPage()}
-            >
-              <ArrowIosForwardOutline className={"arrows"} />
-            </ButtonPagination>
-          )}
-        </Container>
-      )}
-    </>
-  );
-};
+      {
+    // paginas > 1 && (
+      //   <Container className={`pagination-container ${theme}`}>
+      //     pagination.map(pageIndex => pageIndex)
+      //   </Container>
+        // )
+      }
+      </>
+        )
+      };
 
 export default Pagination;
