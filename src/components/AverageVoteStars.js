b8c43d0ff82fda7c3e102675145ea190d8848a53
d-capponi <capponi.daniela@gmail.com>
2020-09-08 18:57:28 -0300

    separo lógica de renderizado

--- src/components/AverageVoteStars.js
+++ src/components/AverageVoteStars.js
@@ -4,26 +4,32 @@ import Star from './Star'
 const AverageVoteStars = ({ voteAverage }) => {
     const rating = Math.round(Math.floor(voteAverage > 1 ? voteAverage / 2 : 0));
     
+    const getAverageVoteStars = (rating) => {
+        let stars = [];
+        if (rating === 0) {
+            stars = [...Array(5)].map((star, i) => 'empty');
+        } else {
+            // Estrellas llenas
+            stars = [...Array(rating)].map((star, i) => "filled");
+            if (rating < 5) {
+                // Estrellas medias si corresponde
+                stars.push(voteAverage % 2 !== 0 ? 'half' : 'empty')
+            }
+            // Estrellas vacías si corresponde
+            if (rating < 4) {
+                [...Array(4 - rating)].map((star, i) => stars.push('empty'))
+            }
+        }
+        return stars;
+    }
+
+    const stars = getAverageVoteStars(rating);
+            
     return (
         voteAverage &&
-        (<div className={ `rating-stars` }>
-            {((rating > 0) &&
-                <>
-                    {[...Array(rating)].map((star, i) => <Star key={i} type={'filled'} className={'average-star'} />)}
-                    <Star type={voteAverage % 2 !== 0 ? 'half' : 'empty'} className={'average-star'} />
-
-                    {rating < 4 && [...Array(4 - rating)].map((star, i) => <Star key={rating + i} type={'empty'} className={'average-star'} />)}
-                </>
-            )
-            }
-            {((rating === 0) &&
-                <>
-                    {[...Array(5)].map((star, i) => <Star key={i} type={'empty'} className={'average-star'} />)}
-                </>
-            )
-            }
-
-        </div>)
+        <div className={ `rating-stars` }>
+        {stars && stars.map((star,i) => <Star key={i} type={star} className={ 'average-star' }/>)}
+        </div>
     )
 }
 
--- src/components/Star.js
+++ src/components/Star.js
@@ -2,8 +2,7 @@ import React from "react";
 import { Star as EmptyStar } from "@styled-icons/fa-regular/Star";
 import { Star as FilledStar } from "@styled-icons/fa-solid/Star";
 import { StarHalfAlt as FilledHalf } from "@styled-icons/fa-solid/StarHalfAlt";
-import { StarHalf as HalfFilled } from "@styled-icons/fa-solid/StarHalf";
-import { StarHalf as HalfEmpty } from "@styled-icons/fa-regular/StarHalf";
+
 
 const Star = ({ type }) => {
   return (
@@ -11,8 +10,6 @@ const Star = ({ type }) => {
       {type === "half" && <FilledHalf className={"star"} />}
       {type === "filled" && <FilledStar className={"star"} />}
       {type === "empty" && <EmptyStar className={"star"} />}
-      {type === "half-empty" && <HalfEmpty className={"star"} />}
-      {type === "half-filled" && <HalfFilled className={"star"} />}
     </>
   );
 };
